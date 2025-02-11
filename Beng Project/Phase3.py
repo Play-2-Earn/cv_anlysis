48a5b799bcd6b89eacb37832d16bf8ee717930a2
Zeiad ALKANDARI <zeiadalkandari2004@gmail.com>
2025-02-11 18:28:15 +0000

    Update and rename Phase3LSBU.py to Phase3.py

--- Beng Project/Phase3.py
+++ Beng Project/Phase3.py
@@ -32,7 +32,7 @@ def extract_keywords(text):
 # Home Route
 @app.route('/')
 def home():
-    return render_template('LSBUPhase3.html')
+    return render_template('LSBUPhase3.html')    # Change this to "LSBUPhase3.html " you can find it in the template sub folder
 
 # Route for JD and CV analysis
 @app.route('/option3', methods=['GET', 'POST'])
